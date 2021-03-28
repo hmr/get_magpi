023c2d504d69f5491a1a86c94e668cbd5d389d24
hmr@entropy3 <hmr@entropy3.local>
2021-03-29 03:27:44 +0900

    Small fix and remove unnecessary file.

--- get_magpi.sh
+++ get_magpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# vim: ft=bash ff=unix fenc=utf-8 ts=2 sw=2 et :
+# vim: ft=sh ff=unix fenc=utf-8 ts=2 sw=2 et :
 
 # get_magpi.sh
 # Downloads all issued MagPi .
@@ -8,38 +8,50 @@
 # AUTHOR: H.Maruyama
 # ORIGIN: 2020-11-27
 
+# Usage: Move to download directory and execute this script.
+
 set -u
 
-DEBUG=0
+DEBUG=1
 URL_LIST_FILE="_url_list.txt"
 
 function dbgout {
   [ "${DEBUG}" = "0" ] || return
 
-  echo $@
+  echo "$@"
 }
 
-declare -a ADD_URL_ARR
 #
 echo "Making URL list."
 while true
 do
   ISSUE=$((${ISSUE:=0} + 1))
   echo "Issue #${ISSUE}"
-  unset URL1 RESPONSE URL2 HTTP_STATUS
+  unset MAGPI_FILE URL1 RESPONSE URL2 HTTP_STATUS
+  MAGPI_FILE=$(printf "MagPi%02d.pdf" "${ISSUE}")
+  if grep -q "${MAGPI_FILE}" "${URL_LIST_FILE}"; then
+    echo "  seems to be found in the download list. skip."
+    continue
+  fi
+
   URL1="https://magpi.raspberrypi.org/issues/${ISSUE}/pdf/download"
   RESPONSE=$(curl -s -w '\n%{http_code}' "${URL1}")
-  URL2=$(echo "${RESPONSE}" | grep "c-link" | grep -o "https://.*\.pdf")
+  # URL2=$(echo "${RESPONSE}" | grep "c-link" | grep -o "https://.*\.pdf")
+  URL2=$(echo "${RESPONSE}" | grep "c-link" | grep -o "/.*\.pdf")
   HTTP_STATUS=$(echo "${RESPONSE}" | tail -n 1)
 
-  # Exit loop if http request was error
-  [ "${HTTP_STATUS}" != "200" ] && break
+  # Exit the loop if http request was error.
+  [ "${HTTP_STATUS}" != "200" ] && echo "  is not issued yet." && break
+  # Abort if URL to download is empty.
+  [ -z "$URL2" ] && echo "  SOMETHING WRONG. ABORT" && exit 1
+  URL2="https://magpi.raspberrypi.org${URL2}"
   # Make output file if it didn't exist.
   [ -e "${URL_LIST_FILE}" ] || touch "${URL_LIST_FILE}"
 
   # Skip if the URL already exists in the file.
+  dbgout "URL2=${URL2}"
   if grep -q "${URL2}" "${URL_LIST_FILE}"; then
-    echo "  exists in the file. skip."
+    echo "  is found in the download list. skip."
     continue
   fi
   dbgout "  HTTP Status: ${HTTP_STATUS}"
